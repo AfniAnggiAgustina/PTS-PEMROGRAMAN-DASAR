6bd06d967644d23c99857ad2df233d46eec71e84
AfniAnggiAgustina <111961875+AfniAnggiAgustina@users.noreply.github.com>
2022-09-28 17:02:33 +0700

    Add files via upload

---
+++ anggi_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama : Afni anggi agustinai"<<endl;
+    cout << "Kelas : X RPL 1"<<endl;
+    cout << "Nilai : 90"<<endl;
+}
